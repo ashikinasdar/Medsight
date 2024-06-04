857ee26306aed85983c1d196dfe2c8d780f58d72
Anisa Chowdhury <129539408+DesDemonaOthelLo@users.noreply.github.com>
2024-06-04 15:17:39 +0600

    update

--- Clinic.cpp
+++ Clinic.cpp
@@ -3,18 +3,14 @@
 #include<string>
 using namespace std;
 
-Clinic :: Clinic(string ct=" ",string st=" ",string clna=" ",string avl=" ",int clno=0)
+Clinic :: Clinic () : stateName(" "), clinicName(" "), availability(" "), ClinicNo(0) {}
+
+Clinic :: Clinic(string st=" ",string clna=" ",string avl=" ",string clno=" ")
 {
-    cityName = ct;
     stateName = st;
     clinicName = clna;
     availability = avl;
     ClinicNo = clno;
-}
-
-void Clinic :: setCity(string ct)
-{
-    cityName = ct;
 }
 
 void Clinic :: setState(string st)
@@ -29,22 +25,12 @@ void Clinic :: setClinicName(string clna)
 
 void Clinic :: setAvailability(string avl)
 {
-    availability = avl;
+    availability = "24 hours";
 }
 
-void Clinic :: setCity(string ct)
-{
-    cityName = ct;
-}
-
-void Clinic :: setClinicNo(int clno)
+void Clinic :: setClinicNo(string clno)
 {
     ClinicNo = clno;
-}
-
-string Clinic :: getCity() const
-{
-    return cityName;
 }
 
 string Clinic :: getState() const
@@ -62,7 +48,75 @@ string Clinic :: getAvailability() const
     return availability;
 }
 
-int Clinic :: getClinicNo() const
+string Clinic :: getClinicNo() const
 {
     return ClinicNo;
+}
+
+void Clinic :: displayclinic() const
+{
+    if(stateName == "Johor" || stateName=="johor")
+    {
+        Clinic("Johor","Sothern Medical Clinic","24 hours","07 336 1731");
+    }
+    else if(stateName == "Sarawak" || stateName=="sarawak")
+    {
+        Clinic("Sarawak","Kotaraya Clinic","24 hours","08 223 2177");
+    }
+    else if(stateName == "Sabah" || stateName=="sabah")
+    {
+        Clinic("Sabah","Klinik Butiza Alamesra","24 hours","08 840 2613");
+    }
+    else if(stateName == "Penang" || stateName=="penang")
+    {
+        Clinic("Penang","Klinik Metrocare","24 hours","04 505 4365");
+    }
+    else if(stateName == "Selangor" || stateName=="selangor")
+    {
+        Clinic("Selangor","Klinik Anda","24 hours","01 337 4398");
+    }
+    else if(stateName == "Kedah" || stateName=="kedah")
+    {
+        Clinic("Kedah","Klinik Metrocare","24 hours","04 771 5052");
+    }
+    else if(stateName == "Perak" || stateName=="perak")
+    {
+        Clinic("Perak","Klinik Zabrina","24 hours","05 282 6771");
+    }
+    else if(stateName == "Kelantan" || stateName=="kelantan")
+    {
+        Clinic("Sarawak","Klinik Ikhtiyar","24 hours","09 791 7117");
+    }
+    else if(stateName == "Sabah" || stateName=="sabah")
+    {
+        Clinic("Sabah","Klinik Butiza Alamesra","24 hours","08 840 2613");
+    }
+    else if(stateName == "Pahang" || stateName=="pahang")
+    {
+        Clinic("Pahang","Klinik Famili","24 hours","01 256 93695");
+    }
+    else if(stateName == "Terengganu" || stateName=="terengganu")
+    {
+        Clinic("Terengganu","Klinik Rohana & Seripah","24 hours","09 623 0369");
+    }
+    else if(stateName == "Negeri Sembilan" || stateName=="negeri sembilan")
+    {
+        Clinic("Negeri Sembilan","Klinik Anda","24 hours","06 675 8668");
+    }
+    else if(stateName == "Melaka" || stateName=="melaka")
+    {
+        Clinic("Melaka","Klinik Bukit Katil","24 hours","06 252 2000");
+    }
+    else if(stateName == "Perlis" || stateName=="perlis")
+    {
+        Clinic("Perlis","Klinik Anda Kangar","24 hours","01 222 97831");
+    }
+    else if(stateName == "Kualalumpur" || stateName=="kualalumpur")
+    {
+        Clinic("Kualalumpur","Klinik Damar","24 hours","03 414 6000");
+    }
+    cout << "Clinic Name: " << getClinicName() << endl;
+    cout << "State: " << getState() << endl;
+    cout << "Availablity: " << getAvailability() << endl;
+    cout << "Contact no: " << getClinicNo() << endl;
 }
